4bcf4ddb95bdbf236a40ce5845370d8cbac317b9
Jefferson Charles <xenoblade61097@gmail.com>
2023-04-26 11:46:41 -0400

    Made Modifications to 7-Segment Program.

--- src/7-Segment-Program.py
+++ src/7-Segment-Program.py
@@ -8,10 +8,10 @@ Y = LED(22)
 Z = LED(23)
 	
 while True:
-       #Here we are asking the user to enter a color from the color wheel to display the number of that color.
-        user_input = input("Please enter a color from the color wheel to display a number corresponding to that color: ")
+      #Here we are asking the user to enter a color from the color wheel to display the number of that color.
+      user_input = input("Please enter a color from the color wheel to display a number corresponding to that color: ")
       
-       if user_input.lower() == 'black':
+      if user_input.lower() == 'black':
             Z.off()
             Y.off()
             X.off()
@@ -19,52 +19,53 @@ while True:
             print("Displaying the number 0!")
 
       elif user_input.lower() == 'green':
-               Z.off()
-               Y.off()
-               X.off()
-               W.on()
-               print("Displaying the number 1!")
+            Z.off()
+            Y.off()
+            X.off()
+            W.on()
+            print("Displaying the number 1!")
 
       elif user_input.lower() == 'blue':
-               Z.off()
-               Y.off()
-               X.on()
-               W.off()
-               print("Displaying the number 2!")
+            Z.off()
+            Y.off()
+            X.on()
+            W.off()
+            print("Displaying the number 2!")
 
       elif user_input.lower() == 'cyan':
-               Z.off()
-               Y.off()
-               X.on()
-               W.on()
-               print("Displaying the number 3!")
+            Z.off()
+            Y.off()
+            X.on()
+            W.on()
+            print("Displaying the number 3!")
 
       elif user_input.lower() == 'orange':
-               Z.off()
-               Y.on()
-               X.off()
-               W.off()
-               print("Displaying the number 4!")
+            Z.off()
+            Y.on()
+            X.off()
+            W.off()
+            print("Displaying the number 4!")
 
       elif user_input.lower() == 'yellow':
-               Z.off()
-               Y.on()
-               X.off()
-               W.on()
-               print("Displaying the number 5!")
+            Z.off()
+            Y.on()
+            X.off()
+            W.on()
+            print("Displaying the number 5!")
 
       elif user_input.lower() == 'red':
-               Z.off()
-               Y.on()
-               X.on()
-               W.off()
-               print("Displaying the number 6!")
+            Z.off()
+            Y.on()
+            X.on()
+            W.off()
+            print("Displaying the number 6!")
 
       elif user_input.lower() == 'white':
-               Z.off()
-               Y.on()
-               X.on()
-               W.on()
-               print("Displaying the number 7!")
+            Z.off()
+            Y.on()
+            X.on()
+            W.on()
+            print("Displaying the number 7!")
+            
       else:
             print("The color you have chosen is not a color on the color wheel. Please enter a color on the color wheel.")
