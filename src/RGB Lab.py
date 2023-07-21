e0a8011a0c7712fc45e6ee7fe9db8142ce3214bf
Jeffersonszn43 <131828443+Jeffersonszn43@users.noreply.github.com>
2023-07-21 17:12:46 -0400

    Update RGB Lab.py

--- src/RGB Lab.py
+++ src/RGB Lab.py
@@ -7,19 +7,26 @@ factory = PiGPIOFactory()
 
 servo = AngularServo(23, min_angle = 0, max_angle = 180, pin_factory = factory)  #Pin 16
 
-#We are going from lsb to msb
-W = LED(17)
-X = LED(27)
-Y = LED(22)
-Z = LED(23)
+#Here are the variables for the 7-Segment display. We are going from lsb to msb.
+W = LED(17)  #Pin 11
+X = LED(27)  #Pin 13
+Y = LED(22)  #Pin 15
+Z = LED(5)  #Pin 29
 
-Red = LED(17)   #Pin 11
-Green = LED(27)  #Pin 13
-Blue = LED(22)  #Pin 15
-White = LED(23)   #Pin 16
+#Here are the variables for the LEDs.
+Red = LED(6)   #Pin 31
+Green = LED(26)  #Pin 37
+Blue = LED(25)  #Pin 22
+White = LED(16)   #Pin 36
 Yellow = LED(24)   #Pin 18
 
 while True:
-     #Here we are asking the user to enter a color on the wheel and depending on what color the user enters, the servo will turn to that color.
-     user_input = input("Please enter a color on the color wheel to turn to that color:  ")
+     #Here we are asking the user to enter a color from the color wheel.
+     user_input = input("Please enter a color from the color wheel:  ")
+
+     if user_input.lower() == 'black':
+          print("Displaying the color Black.")
+          servo.angle = 0
+          
+
 
